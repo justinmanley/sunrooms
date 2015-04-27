99bbfd12ec284aff0ea02016c398a1787f056a36
Justin Manley <manleyjster@gmail.com>
2015-04-27 07:05:13 +0000

    Add setup file to install postgres and postgis and set up the appropriate database roles. Refine load.sh to handle the schema for the foia database.

--- scripts/load.sh
+++ scripts/load.sh
@@ -1,6 +1,14 @@
+# Data source files
+foia_schema="data/foia-22606/foia.sql"
+cc_tax_assessors_foia="data/foia-22606/FOI22606.CSV" 
+building_footprints="data/building-footprints/buildings"
+
 # Load data from FOIA request into postgres.
 # Note that the schema for the foia table was already set up using csvsql from csvkit.
-cat FOI22606.CSV | psql sunrooms -c "COPY foia FROM stdin WITH (FORMAT CSV, HEADER true)"
+psql sunrooms -f $foia_schema
+cat $cc_tax_assessors_foia \
+    | sed 's/$//' \
+    | psql sunrooms -c "COPY foia FROM stdin WITH (FORMAT CSV, HEADER true)"
 
 # Load building footprints dataset from the City of Chicago data portal into postgres.
-shp2pgsql -D -W "LATIN1" buildings footprints | psql sunrooms
+shp2pgsql -D -W "LATIN1" $building_footprints footprints | psql sunrooms
