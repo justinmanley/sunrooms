d1d5e116fb6db9f5c5c29deaf1c89b750a2b2443
Justin Manley <manleyjster@gmail.com>
2015-07-08 03:48:26 +0000

    Add latlng geometry column to ccaddresses table w/ check for NULL lat and lng values.

--- ccaddresses.sql
+++ ccaddresses.sql
@@ -1,3 +1,12 @@
 ALTER TABLE ccaddresses ADD COLUMN address varchar(171);
-UPDATE ccaddresses SET address = concat_ws(' ', addrnocom, stnamecom, uspspn, \
-    uspsst, zip5);
+UPDATE ccaddresses SET address = concat_ws(' ', addrnocom, stnamecom, uspspn, uspsst, zip5);
+
+SELECT AddGeometryColumn('ccaddresses', 'latlng', 4326, 'POINT', 2);
+UPDATE ccaddresses SET latlng = CASE WHEN longitude IS NULL OR latitude IS NULL
+    THEN ST_SetSRID(ST_MakePoint(0.0, 0.0), 4326)
+    ELSE ST_GeomFromText( 
+        'POINT(' || concat_ws(' ', longitude, latitude) || ')', 4326
+    )
+    END;
+
+
